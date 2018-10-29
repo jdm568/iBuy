4d662c84616c0385d01ec51b9daaacc1b120598e
JDMitch <jdm568@nau.edu>
2018-10-29 15:24:05 -0700

    added button functionality

--- Categories.java
+++ Categories.java
@@ -1,13 +1,59 @@
 package edu.nau.ibuy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Categories extends AppCompatActivity {
+    private Button food;
+    private Button household;
+    private Button hygiene;
+    private Button other;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        food = (Button) findViewById(R.id.foodButton);
+        household = (Button) findViewById(R.id.houseHoldButton);
+        hygiene = (Button) findViewById(R.id.hygieneButton);
+        other = (Button) findViewById(R.id.otherButton);
+
+        food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openShoppingBag();
+            }
+        });
+
+        household.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openShoppingBag();
+            }
+        });
+
+        hygiene.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openShoppingBag();
+            }
+        });
+
+        other.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openShoppingBag();
+            }
+        });
+
+    }
+
+    public void openShoppingBag() {
+        Intent intent = new Intent(this, ShoppingBag.class);
+        startActivity(intent);
     }
 }
--- LogIn.java
+++ LogIn.java
@@ -1,13 +1,32 @@
 package edu.nau.ibuy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogIn extends AppCompatActivity {
+    private Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        signUp = (Button) findViewById(R.id.signUp);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCategoriesPage();
+
+            }
+        });
+
+    }
+    public void openCategoriesPage() {
+        Intent intent = new Intent(this, Categories.class);
+        startActivity(intent);
     }
 }
