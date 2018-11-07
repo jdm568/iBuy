c112d92e6c42c702b0f63ba56c6c896fb9f373ad
mlg389 <36056922+mlg389@users.noreply.github.com>
2018-11-07 15:35:41 -0700

    Added account button

--- ListOfFood.java
+++ ListOfFood.java
@@ -1,13 +1,32 @@
 package edu.nau.ibuy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListOfFood extends AppCompatActivity {
+    private Button account;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_food);
+
+        account = (Button) findViewById(R.id.account);
+
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAccountPreferences();
+            }
+        });
+    }
+
+    public void openAccountPreferences() {
+        Intent intent = new Intent(this, AccountPreferences.class);
+        startActivity(intent);
     }
 }
